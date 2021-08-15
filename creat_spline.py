4d0b062266cd9d7ebd23bb8c0054655ac3b1d03f
SuperC001 <591465386@qq.com>
2021-08-15 12:05:29 +0800

    对控制点进行了参数化, 可以进行批量添加

--- creat_spline.py
+++ creat_spline.py
@@ -6,20 +6,26 @@ part1 = partdocument1.part
 bodies1 = part1.bodies
 body1 = bodies1.add()
 sketches1 = body1.sketches
+
+
+# 将yz设置为参考平面
 rf = part1.OriginElements.planeyz
 sketch1 = sketches1.add(rf)
 factory2D1 = sketch1.openedition()
 
-# 创建样条曲线的控制点
-controlpoint2D1 = factory2D1.createcontrolpoint(185.371460, 34.484993)
-controlpoint2D2 = factory2D1.createcontrolpoint(77.456879, -47.910454)
-controlpoint2D3 = factory2D1.createcontrolpoint(-76.406647, 43.435303)
-controlpoint2D4 = factory2D1.createcontrolpoint(-166.204132, 34.484993)
+# 一条曲线中控制点个数
+control_point_num = 4
 
-# 创建控制点列表，为创建样条线提供参数
-controlpoints = [controlpoint2D1, controlpoint2D2, controlpoint2D3, controlpoint2D4]
+# 批量实例化控制点,并将其添加到controlpoint2D列表中
+controlpoint2D = []
 
-spline2D1 = factory2D1.createspline(controlpoints)
+# 列表中为控制点参数
+controlpoints_data = [(185.371460, 34.484993),(77.456879, -47.910454),(-76.406647, 43.435303),(-166.204132, 34.484993)]
+for i in range(control_point_num):
+    controlpoint2D.append(factory2D1.createcontrolpoint(controlpoints_data[i][0],controlpoints_data[i][1]))
+
+#使用控制点列表作为参数实例化样条曲线
+spline2D1 = factory2D1.createspline(controlpoint2D)
 
 sketch1.closeedition()
 
